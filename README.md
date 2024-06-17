7fffde42365c72a67b43f8174c2676efb278fad4
Jonathan Cacace <jonathan.cacace@eurecat.org>
2024-06-17 13:01:36 +0200

    tested and readme added

--- README.md
+++ README.md
@@ -1,14 +1,80 @@
-Dependencies:
+# Optitrack listener
 
-apt-get install python3-pip
-pip3 install optirx
+This repository contains a ROS2 package implementing the NatNet _v2.5_ protocol to received Optitrack data and stream over the ROS2 network.
 
-Streaming panel configuration:
-    - Pin Broadcast Frame Data
-    - Stream Rigid body: True
-    - Type: Multicast 
-    - Command port: 1510
-    - Data port: 1511
-    - Local interface: Ip address of the network device communicating with the remote computer (i.e. 172.22.120.80)
-    - Multicast Interface: Ip address of the device receiving data
+### Motivation
+NatNet _v2.5_ protocol is old and it's contained in the Motive software 1.5 (2013). Other packages available online failed to receive data. o t
+
+### Requirements
+- A ROS2 installation (this package has been tested with humble)
+ - Pip3 (you can install it using apt)
+
+	   $ apt-get install python3-pip
+- optirx (you can install it using pip3)
+	
+	  $ pip3 install optirx
+
+### Installation
+Move in your ROS2 workspace
+
+	$ cd ros2_ws/src
+	$ git clone git@github.com:jocacace/ros2_optitrack_listener.git
+
+Compile the workspace
+
+	$ colcon build --symlink-install 
+
+### Execution
+To start the optitrack listener node, you can use a convenient launch file included in the launch folder of the package. 
+
+	$ ros2 launch optitrack_listener optitrack_listener.launch.py 
+
+## Motive configuration
+
+### Network
+Before to start the optitrack node, you should properly configure the Motive software. Here is how to set it.
+
+- Open the _Streaming panel_:
+	- Pin the _Broadcast Frame Data_ checkbox
+	- Set the _Stream Rigid body_ to true
+	- Select the  _Multicast_ in the communication type
+	- Ports: 
+		- Command port: 1510
+		- Data port: 1511
+	- Local interface: this interface must be set to use the one communicating with the remote computer. For example if you have two network devices and only one of them can ping the remote computer, the local interface must be set to that Ip address
+- Multicast Interface: This Ip address must be the one of the device receiving data
+
+### Trackable
+Before to start the data streaming, a set of trackable should be created. You can directly select the markers composing a given trackable. However, after created the object, in the trackable proprieties panel you can set its name and its id. The id is important to properly configure the ROS2 data streaming
+
+### Node configuration
+
+To configure the node a proper configuration file is already contained in the config folder of the package. It is a yaml file 
+
+	optitrack_listener:
+		ros__parameters:
+			fixed_frame: "map"
+			local_interface: '172.22.160.20'
+			rigid_object_list: 'body_2,body_3'
+			trackables:
+				body_2:
+					id: 1
+					name: ground_plan_tool
+				body_3:
+					id: 4
+					name: calibration_wand
+			publish_tf: True
+
+Here we can specify:
+- Fixed frame: the frame toward publish the pose data
+- local_interface: the Ip address where optitrack data are received
+- rigid_object list: this is a list of names. The names are used to initialize the other parameters of the file. For example, here is expected to find the body_2 and body_3 parameter definition. The names included here must be separated by the "," (comma) separator. No matter the space before or after the comma
+- trackables: the list of all the rigid object that can be found into the optitrack data. Each of them has an id, that is the id received via the optitrack network protocol and a name. The name is also used to create a topic publishing data in the ROS2 network
+- publish_tf: set this variable to True to publish data also in tf form, false otherwise
+
+### Output
+For each trackable  (${trackable}) name in the yaml configuration file the following output is generated:
+- /optitrack/${trackable}: a PoseStamped message containing timestamp and pose data of the object
+- A tf data if the publish_tf param is set to true 
+
 
--- optitrack_listener/listener.py
+++ optitrack_listener/listener.py
@@ -4,62 +4,119 @@ from threading import Thread
 import optirx as rx
 import socket
 from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import PoseStamped, TransformStamped, Quaternion, Point
+from tf2_ros import TransformBroadcaster
+from tf2_geometry_msgs import do_transform_pose
 import numpy as np
+import re   
 
 class OptitrackListener( Node ):
     def __init__(self):
         super().__init__('optitrack_listener')
         self.declare_parameter('local_interface',  "192.168.1.13")
         self.ipaddr = self.get_parameter('local_interface').get_parameter_value().string_value
-        
+        self.declare_parameter('fixed_frame', 'world')
+        self.frame = self.get_parameter('fixed_frame').get_parameter_value().string_value
+        self.declare_parameter('rigid_object_list', "")
+        rigid_object_list = self.get_parameter('rigid_object_list').get_parameter_value().string_value
+        split_string = rigid_object_list.split(', ')
+        self.declare_parameter('publish_tf', False)
+        self.publish_tf = self.get_parameter( 'publish_tf').get_parameter_value().bool_value
+        names = rigid_object_list.split(',')
+        self.get_logger().info("Trackable configuration {names}")
+
+        self.id_trackable_dict = {}   # Dictionary: from object id to trackable name
+        self.id_publisher_dict = {}   # Dictionary: from object id to publisher id 
+        self.get_logger().info("Trackable configuration")
+        self.trackable_publishers = []
+        index = 0
+
+        for name in names:
+            name = name.strip()
+            self.declare_parameter(f'trackables.{name}.id', 0)
+            self.declare_parameter(f'trackables.{name}.name', "trackable")
+            id_param = self.get_parameter(f'trackables.{name}.id').get_parameter_value().integer_value
+            name_param = self.get_parameter(f'trackables.{name}.name').get_parameter_value().string_value
+            
+            self.get_logger().info(f"Name: '{name}'")
+            self.get_logger().info(f"id: {id_param}")
+            self.get_logger().info(f"name: {name_param}")
+
+            self.id_trackable_dict[id_param] = name_param
+            self.id_publisher_dict[id_param] = index 
+
+            self.trackable_publishers.append ( self.create_publisher(PoseStamped, "optitrack/" + name_param, 1))
+            index = index+1
+
+
+        self.tf_broadcaster = TransformBroadcaster(self)
         self.thread = Thread(target = self.get_optitrack_data, args = ())
         self.thread.start()
 
+    def pose_to_tf(self, pose_msg, parent_frame, target_frame):
+        # Create a TransformStamped message
+        transform_stamped = TransformStamped()
+        
+        # Set the header of the TF message
+        transform_stamped.header.stamp = pose_msg.header.stamp
+        transform_stamped.header.frame_id = parent_frame  
+        transform_stamped.child_frame_id = target_frame  
+
+        # Extract position and orientation from PoseStamped message
+        position = pose_msg.pose.position
+        orientation = pose_msg.pose.orientation
+        
+        # Set translation and rotation in the TF message
+        transform_stamped.transform.translation.x = position.x
+        transform_stamped.transform.translation.y = position.y
+        transform_stamped.transform.translation.z = position.z
+        transform_stamped.transform.rotation = orientation
+
+        return transform_stamped  
     def get_optitrack_data(self):
         
-        print("Using ipaddr: ", self.ipaddr)
         self.version = (2, 5, 0, 0)  # the latest SDK version
         self.optitrack = rx.mkdatasock(ip_address=self.ipaddr)#(ip_address=get_ip_address(iface))
-        print('Successfully connected to optitrack\n')
-        
-        names = []
-        ids = []
-                
+              
+        ps = PoseStamped()
+
         first = True
         while rclpy.ok():
             try:
                 data = self.optitrack.recv(rx.MAX_PACKETSIZE)
             except socket.error:
-                
-                print('Failed to receive packet from optitrack')
-      
+                self.get_logger().info(f"Failed to receive packet from optitrack")
+
             packet = rx.unpack(data, version=self.version)
-            print(packet)
             if first == True:
-                print('NatNet version received: ' + str(self.version))
+                self.get_logger().info(f"NatNet version received {self.version}")
                 first = False
+                
             if type(packet) is rx.SenderData:
                 self.version = packet.natnet_version
-                print('NatNet version received: ' + str(self.version))
+                self.get_logger().info(f"NatNet version received {self.version}")
 
             if type(packet) in [rx.SenderData, rx.ModelDefs, rx.FrameOfData]:
-                # Optitrack gives the position of the centroid.
-                print("If")
-                '''
-                array_msg = RigidBodyArray()
-                '''
                 for i, rigid_body in enumerate(packet.rigid_bodies):
                     body_id = rigid_body.id
                     pos_opt = np.array(rigid_body.position)
                     rot_opt = np.array(rigid_body.orientation)
+                    
+                    ps.header.stamp = self.get_clock().now().to_msg()  
+                    ps.header.frame_id = self.frame
+                    ps.pose.position = Point(x=pos_opt[0], y=pos_opt[1], z=pos_opt[2])  
+                    ps.pose.orientation = Quaternion(x=rot_opt[0], y=rot_opt[1], z=rot_opt[2], w=rot_opt[3])  
 
-                    print( body_id )
-                    print( pos_opt )
-                    print( rot_opt )
-                    print( rigid_body.tracking_valid )
-                    print( rigid_body.mrk_mean_error )
-                    print("Marker size: ", len(rigid_body.markers ))
-                    print( rigid_body.markers )
+
+                    if body_id in self.id_publisher_dict:
+                        self.trackable_publishers[ self.id_publisher_dict[body_id] ].publish( ps )
+                        if( self.publish_tf):
+                            transform_stamped = self.pose_to_tf(ps, self.frame, self.id_trackable_dict[body_id] )                    
+                            self.tf_broadcaster.sendTransform(transform_stamped)
+                    else: 
+                        self.get_logger().warn(f"Id: {body_id} not present in the configuration file")
 
 
 def main(args=None):
